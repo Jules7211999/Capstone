c0ad5f971b61aa473c866dfa15ca9ec35a05f088
Jules Leomel <julesparcon1999@gmail.com>
2021-09-15 14:41:52 +0800

    uservue restored

---
+++ resources/js/components/admin/UserShow.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="w-100 h-100">
+    <div class>
+         <img :src="'/img/'+profile.profile_image" alt="" class="w-25">
+    </div>
+   
+    <span>{{profile.name}}</span>
+    <span>{{profile.address}}</span>
+    <span>{{profile.phone_number}}</span>
+    <span>{{profile.birthdate}}</span>
+    <span>{{profile.gender}}</span>
+    <span>{{profile.email_verified_at}}</span>
+    <span>{{profile.email}}</span>
+    <span>{{profile.created_at}}</span>
+</div>
+    
+</template>
+
+<script>
+export default {
+    props:['data'],
+    data(){
+        return{
+         profile:this.data
+        }
+    },
+   mounted(){
+       
+   }
+}
+</script>
+
+<style scoped>
+span{
+    font-weight: bold;
+}
+</style>
