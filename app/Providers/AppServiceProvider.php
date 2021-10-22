2d22249fd46cb66acd0cf9b4b8813ae4bf994022
Jules Leomel <julesparcon1999@gmail.com>
2021-10-22 21:44:34 +0800

    asdfads

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // don't forget to comment this when on localhost
-        if(config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        // if(config('app.env') === 'production') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
