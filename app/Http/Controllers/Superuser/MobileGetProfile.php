bc0aa0d581cae1a4c01da001f72fdc7c341fa57f
Jules Leomel <julesparcon1999@gmail.com>
2021-10-27 08:06:52 +0800

    test heroku endpoint

--- app/Http/Controllers/Superuser/MobileGetProfile.php
+++ app/Http/Controllers/Superuser/MobileGetProfile.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\Superuser;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class MobileGetProfile extends Controller
 {
     public function index($id){
-        return $id;
+        $user = User::find($id);
+        return response()->json([
+            $user->name,
+            $user->username,
+            $user->marital_status,
+            $user->profile_image,
+            $user->gender,
+            $user->birthdate,
+
+        ]);
     }
 }
