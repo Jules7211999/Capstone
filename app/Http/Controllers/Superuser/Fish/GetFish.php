f914e8d8c32b67f7a6079aca478c9a9d542add89
Jules Leomel <julesparcon1999@gmail.com>
2021-11-01 07:25:46 +0800

    asdf

--- app/Http/Controllers/Superuser/Fish/GetFish.php
+++ app/Http/Controllers/Superuser/Fish/GetFish.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Superuser\Fish;
 
 use App\Http\Controllers\Controller;
-use App\Models\Fish;
+use App\Models\fish;
 use Illuminate\Http\Request;
 
 class GetFish extends Controller
