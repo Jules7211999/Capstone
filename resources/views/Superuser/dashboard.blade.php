8fe7019d38cfea0bb514e0f48447bbef87840062
Jules Leomel <julesparcon1999@gmail.com>
2021-11-06 07:56:09 +0800

    test heroku:

--- resources/views/Superuser/dashboard.blade.php
+++ resources/views/Superuser/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-<admin-container-component role="{{auth()->user()->role}}" authname="{{auth()->user()->name}}" nav="Dashboards">
+<admin-container-component role="{{auth()->user()->role}}" authname="{{auth()->user()->name}}" nav="Dashboard">
   <dashboard-component user="{{$user}}" email="{{$email}}" coord="{{$coord}}" sos="{{$sos}}"/>
 </admin-container-component>
 @endsection
