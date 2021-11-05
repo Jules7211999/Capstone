d4e23b0de96c96ba69259cfa2c5b0977101ad811
Jules Leomel <julesparcon1999@gmail.com>
2021-11-06 07:54:42 +0800

    test hjeroasdf

--- resources/views/Superuser/dashboard.blade.php
+++ resources/views/Superuser/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('content')
-<admin-container-component role="{{auth()->user()->role}}" authname="{{auth()->user()->name}}" nav="Dashboard">
+<admin-container-component role="{{auth()->user()->role}}" authname="{{auth()->user()->name}}" nav="Dashboards">
   <dashboard-component user="{{$user}}" email="{{$email}}" coord="{{$coord}}" sos="{{$sos}}"/>
 </admin-container-component>
 @endsection
