33ee9d5ea5873577cad48fccfa51ffc96bfb816e
Jules Leomel <julesparcon1999@gmail.com>
2021-11-29 11:29:19 +0800

    asdf

--- app/Http/Controllers/User/TrackLocationController.php
+++ app/Http/Controllers/User/TrackLocationController.php
@@ -12,13 +12,14 @@ class TrackLocationController extends Controller
     public function store(Request $request){
         $dtm = Carbon::now(); 
         $month = $dtm->format('m');
+        $month_name = $dtm -> format('M');
         Coordinates::create([
             'latitude' => $request->lat,
             'longitude' => $request->long,
             'user_id' => $request -> user,
             'datetimezone' => $dtm,
-            'month_name' => $dtm -> monthName,
-            'month_id' => $dtm -> $month,
+            'month_name' => $month_name,
+            'month_id' =>  $month,
             'day_of_week' => $dtm -> shortLocaleDayOfWeek
         ]);
     }
