5ccc45982c3da0bf2895d6f68f5d5badd3c6be9d
Jules Leomel <julesparcon1999@gmail.com>
2021-11-29 16:15:18 +0800

    asdf

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,12 +12,12 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{secure_asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{asset('css/app.css') }}">
+        <!-- <link rel="stylesheet" href="{{asset('css/app.css') }}"> -->
 
 
         <!-- Scripts -->
         <script src="{{ secure_asset('js/app.js') }}" defer></script>
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     </head>
     <style>
              #logo{
