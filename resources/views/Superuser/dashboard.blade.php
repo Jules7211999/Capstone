354cf1c0600202141df95f6b4e1b9d56ff995bd4
Jules Leomel <julesparcon1999@gmail.com>
2021-11-30 09:49:28 +0800

    asdfs

--- resources/views/Superuser/dashboard.blade.php
+++ resources/views/Superuser/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <admin-container-component role="{{auth()->user()->role}}" authname="{{auth()->user()->name}}" nav="Dashboard">
-  {{$$city}}
+  {{$city}}
   <dashboard-component species="{{$fish}}" catchk="{{$tkilos}}" user= {{$user}} fisherman = {{$fisherman}} admin = {{$admin}} sos = {{$sos}} sosd = {{$sosd}} sosp = {{$sosp}} coord = {{$coord}} barangay = {{$barangay}} city={{$city}} />
 </admin-container-component>
 @endsection
