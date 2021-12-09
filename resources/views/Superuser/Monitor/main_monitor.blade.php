a63127107bd252473c31e9a4f7101ef38f7beb93
Jules Leomel <julesparcon1999@gmail.com>
2021-12-09 08:48:03 +0800

    asdfs

--- resources/views/Superuser/Monitor/main_monitor.blade.php
+++ resources/views/Superuser/Monitor/main_monitor.blade.php
@@ -4,17 +4,30 @@
 <div class="p-2 mb-5 bh-white row w-100 d-flex justify-content-center">
 <img src="https://capstone-salvador-bucket.s3.us-east-2.amazonaws.com/{{$fish->image}}" alt="" class="w-50">
 </div>
-<div class="p-2 mb-5 bh-white row w-100">
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->phylum}}</div>
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->subphylum}}</div>
+<div class="p-1 mb-2 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold"><span>Phylum</span></div>
+<div class="col d-flex justify-content-center font-weight-bold"><span>Subphylum</span></div>
 </div>
-<div class="p-2 mb-5 bh-white row w-100">
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->superclass}}</div>
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->class}}</div>
+<div class="p-1 mb-5 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->phylum}}</div>
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->subphylum}}</div>
 </div>
-<div class="p-2 mb-5 bh-white row w-100">
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->common_name}}</div>
-<div class="col d-flex justify-content-center font-weight-bold">{{$fish->local_name}}</div>
+<div class="p-1 mb-2 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold">Superclass</div>
+<div class="col d-flex justify-content-center font-weight-bold">Class</div>
+</div>
+<div class="p-1 mb-5 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->superclass}}</div>
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->class}}</div>
+</div>
+<div class="p-1 mb-2 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold">Common Name</div>
+<div class="col d-flex justify-content-center font-weight-bold">Local Name</div>
+</div>
+
+<div class="p-1 mb-5 bh-white row w-100">
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->common_name}}</div>
+<div class="col d-flex justify-content-center font-weight-bold text-secondary">{{$fish->local_name}}</div>
 </div>
 
 
